6075e3d85ec38bfe7e166fdfb05539dc0ae9195f
Jared Grimes <jaredlancegrimes@gmail.com>
2025-01-09 13:41:21 -0600

    migrated the pictures in cells

--- components/pkmn-table.columns.tsx
+++ components/pkmn-table.columns.tsx
@@ -2,11 +2,22 @@
 
 import { ColumnDef } from '@tanstack/react-table';
 import { PokemonTableEntry } from './pkmn-table.types';
+import Image from 'next/image';
 
 export const columns: ColumnDef<PokemonTableEntry>[] = [
   {
     accessorKey: 'index',
     header: 'Dex',
+    cell: ({ row }) => (
+      <Image
+        src={`https://raw.githubusercontent.com/keldaanCommunity/SpriteCollab/master/portrait/${row.getValue(
+          'index'
+        )}/Normal.png`}
+        alt={row.getValue('name')}
+        width={40}
+        height={40}
+      />
+    ),
   },
   {
     accessorKey: 'name',
@@ -15,10 +26,36 @@ export const columns: ColumnDef<PokemonTableEntry>[] = [
   {
     accessorKey: 'tier',
     header: 'Tier',
+    cell: ({ row }) => (
+      <div className='flex'>
+        {[...Array.from(Array(row.getValue('tier')))].map((_, idx) => (
+          <Image
+            key={`${row.getValue('index')}-star-${idx}`}
+            src={`https://raw.githubusercontent.com/keldaanCommunity/pokemonAutoChess/c2b4974c4b1807c24c62020b0de7e5f9db532b1f/app/public/src/assets/ui/star.svg`}
+            alt='star'
+            width={16}
+            height={16}
+          />
+        ))}
+      </div>
+    ),
   },
   {
     accessorKey: 'types',
     header: 'Types',
+    cell: ({ row }) => (
+      <div className='flex'>
+        {(row.getValue('types') as string[]).map(type => (
+          <Image
+            key={`${row.getValue('index')}-type-${type}`}
+            src={`https://raw.githubusercontent.com/keldaanCommunity/pokemonAutoChess/refs/heads/master/app/public/src/assets/types/${type}.svg`}
+            alt={type}
+            width={32}
+            height={32}
+          />
+        ))}
+      </div>
+    ),
   },
   {
     accessorKey: 'hp',
--- components/pkmn-table.tsx
+++ components/pkmn-table.tsx
@@ -18,7 +18,6 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table';
-import { PokemonTableContentCell } from './pkmn.table.cell-content';
 
 interface PokemonTableProps {
   data: PokemonTableEntry[];
@@ -61,7 +60,7 @@ export function PokemonTable(props: PokemonTableProps) {
             >
               {row.getVisibleCells().map(cell => (
                 <TableCell key={cell.id}>
-                  <PokemonTableContentCell cell={cell} />
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </TableCell>
               ))}
             </TableRow>
@@ -76,50 +75,6 @@ export function PokemonTable(props: PokemonTableProps) {
             </TableCell>
           </TableRow>
         )}
-        {/* {sortedPokemon.map(pkmn => (
-          <TableRow key={pkmn.index}>
-            <TableCell>
-              <Image
-                src={`https://raw.githubusercontent.com/keldaanCommunity/SpriteCollab/master/portrait/${pkmn.index}/Normal.png`}
-                alt={pkmn.name}
-                width={40}
-                height={40}
-              />
-            </TableCell>
-            <TableCell>{pkmn.name}</TableCell>
-            <TableCell>
-              <div className='flex'>
-                {[...Array.from(Array(pkmn.tier))].map((_, idx) => (
-                  <Image
-                    key={`${pkmn.index}-star-${idx}`}
-                    src={`https://raw.githubusercontent.com/keldaanCommunity/pokemonAutoChess/c2b4974c4b1807c24c62020b0de7e5f9db532b1f/app/public/src/assets/ui/star.svg`}
-                    alt='star'
-                    width={16}
-                    height={16}
-                  />
-                ))}
-              </div>
-            </TableCell>
-            <TableCell className='flex'>
-              {[pkmn.type1, pkmn.type2, pkmn.type3, pkmn.type4].map(
-                type =>
-                  type.length > 1 && (
-                    <Image
-                      key={`${pkmn.index}-type-${type}`}
-                      src={`https://raw.githubusercontent.com/keldaanCommunity/pokemonAutoChess/refs/heads/master/app/public/src/assets/types/${type}.svg`}
-                      alt={pkmn.name}
-                      width={32}
-                      height={32}
-                    />
-                  )
-              )}
-            </TableCell>
-            <TableCell className='text-right'>{pkmn.hp}</TableCell>
-            <TableCell className='text-right'>{pkmn.attack}</TableCell>
-            <TableCell className='text-right'>{pkmn.defense}</TableCell>
-            <TableCell className='text-right'>{pkmn.specialDefense}</TableCell>
-          </TableRow>
-        ))} */}
       </TableBody>
     </Table>
   );
