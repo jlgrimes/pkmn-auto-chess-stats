385b722d3389f3de6bb22a72961250bbf25169fa
Jared Grimes <jaredlancegrimes@gmail.com>
2025-01-14 18:51:00 -0600

    made the tab bar pretty actually

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,16 @@ import { Roboto_Mono } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-  NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] });
 import './globals.css';
-import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
 
 export const metadata: Metadata = {
   title: 'pokemon auto chess stats',
@@ -31,28 +27,37 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={robotoMono.className}>
-        <div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 space-y-2'>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href='/' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Home
-                  </NavigationMenuLink>
-                </Link>
-                <Link href='/pokemon' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Pokemon
-                  </NavigationMenuLink>
-                </Link>
-                <Link href='/weather' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Weather
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+        <div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 space-y-4'>
+          <div className='space-y-2'>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <Link href='/' legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Home
+                    </NavigationMenuLink>
+                  </Link>
+                  <Link href='/pokemon' legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Pokemon
+                    </NavigationMenuLink>
+                  </Link>
+                  <Link href='/weather' legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      Weather
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+            <Separator />
+          </div>
           {children}
         </div>
         <Analytics />
