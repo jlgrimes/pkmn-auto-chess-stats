e77165e9bbea389190bf190cabe021203a51c7be
Jared Grimes <jaredlancegrimes@gmail.com>
2025-01-15 18:30:16 -0600

    rune protect

--- components/pkmn-table/ability-cell.tsx
+++ components/pkmn-table/ability-cell.tsx
@@ -13,6 +13,7 @@ import { Flinch } from '../icons/flinch';
 import { Charm } from '../icons/charm';
 import { Sleep } from '../icons/sleep';
 import { PP } from '../icons/pp';
+import { RuneProtect } from '../icons/rune-protect';
 
 interface AbilityCellProps {
   abilityPower: number;
@@ -20,76 +21,80 @@ interface AbilityCellProps {
 }
 
 export const AbilityCell = (props: AbilityCellProps) => {
+  const shouldRenderAP =
+    props.abilityPower > 0 || props.description.includes('SPECIAL');
+  const shouldRenderATK =
+    /((Deal|dealing|take) (\[[\dA-Za-z,]+\]|150%) (PHYSICAL|TRUE|ATK))|(% of (its )?ATK. to)/i.test(
+      props.description
+    );
+  const shouldRenderAPNum = props.abilityPower > 0;
   return (
-    <div className='flex flex-row space-x-2 justify-between'>
-      <div className='flex flex-row space-x-2'>
-        {(props.abilityPower > 0 || props.description.includes('SPECIAL')) && (
-          <AbilityPower />
-        )}
-        {/((Deal|dealing|take) (\[[\dA-Za-z,]+\]|150%) (PHYSICAL|TRUE|ATK))|(% of (its )?ATK.)/i.test(
-          props.description
-        ) && <Attack />}
-        {props.abilityPower > 0 && <span>{props.abilityPower}</span>}
-        {props.description.includes('POISONNED') && <Poison />}
-        {props.description.includes('CURSE') && <Curse />}
-        {props.description.includes('FLINCH') && <Flinch />}
-        {props.description.includes('CHARM') && <Charm />}
-        {props.description.includes('SLEEP') && <Sleep />}
-      </div>
-      <div className='flex flex-row space-x-2'>
-        {props.description.includes('PROTECT') && <Protect />}
-        {/(Increase ATK_SPEED)|(Gain \[[\dA-Za-z,]+\]% ATK_SPEED)/i.test(
-          props.description
-        ) && (
-          <div className='flex flex-row'>
-            <AttackSpeed />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {(props.description.includes('critical hit') ||
-          props.description.includes('CRIT_CHANCE')) && (
-          <div className='flex flex-row'>
-            <CritChance />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {/(Gain|Grant) \[[\dA-Za-z,]+\] SHIELD/i.test(props.description) && (
-          <div className='flex flex-row'>
-            <Shield />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {(props.description.includes(`Increase the user's ATK`) ||
-          props.description.includes(`Increase the ATK`) ||
-          props.description.includes(`Increase ATK `) ||
-          props.description.includes(`increase their ATK `) ||
-          props.description.includes('and ATK by')) && (
-          <div className='flex flex-row'>
-            <Attack />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {/((Gain) (\[[\dA-Za-z,]+\]) DEF)|(Increase the user's DEF)/i.test(
-          props.description
-        ) && (
-          <div className='flex flex-row'>
-            <Defense />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {/(heal|recovers|(increase max hp by))/i.test(props.description) && (
-          <div className='flex flex-row'>
-            <HP />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-        {/((Restore) (\[[\dA-Za-z,=\.]+\]) PP)/i.test(props.description) && (
-          <div className='flex flex-row'>
-            <PP />
-            <ChevronsUpIcon className='h-4 w-4' />
-          </div>
-        )}
-      </div>
+    <div className='flex flex-row space-x-4 '>
+      {(shouldRenderAP || shouldRenderATK) && (
+        <div className='flex flex-row space-x-1'>
+          {shouldRenderAP && <AbilityPower />}
+          {shouldRenderATK && <Attack />}
+          {shouldRenderAPNum && <span>{props.abilityPower}</span>}
+        </div>
+      )}
+      {/(Increase ATK_SPEED)|(Gain \[[\dA-Za-z,]+\]% ATK_SPEED)/i.test(
+        props.description
+      ) && (
+        <div className='flex flex-row'>
+          <AttackSpeed />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {(props.description.includes('critical hit') ||
+        props.description.includes('CRIT_CHANCE')) && (
+        <div className='flex flex-row'>
+          <CritChance />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {/(Gain|Grant) \[[\dA-Za-z,]+\] SHIELD/i.test(props.description) && (
+        <div className='flex flex-row'>
+          <Shield />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {(props.description.includes(`Increase the user's ATK`) ||
+        props.description.includes(`Increase the ATK`) ||
+        props.description.includes(`Increase ATK `) ||
+        props.description.includes(`increase their ATK `) ||
+        props.description.includes('and ATK by')) && (
+        <div className='flex flex-row'>
+          <Attack />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {/((Gain) (\[[\dA-Za-z,]+\]) DEF)|(Increase the user's DEF)/i.test(
+        props.description
+      ) && (
+        <div className='flex flex-row'>
+          <Defense />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {/(heal|recovers|(increase max hp by))/i.test(props.description) && (
+        <div className='flex flex-row'>
+          <HP />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {/((Restore) (\[[\dA-Za-z,=\.]+\]) PP)/i.test(props.description) && (
+        <div className='flex flex-row'>
+          <PP />
+          <ChevronsUpIcon className='h-4 w-4' />
+        </div>
+      )}
+      {props.description.includes('POISONNED') && <Poison />}
+      {props.description.includes('CURSE') && <Curse />}
+      {props.description.includes('FLINCH') && <Flinch />}
+      {props.description.includes('CHARM') && <Charm />}
+      {props.description.includes('SLEEP') && <Sleep />}
+      {props.description.includes('PROTECT') && <Protect />}
+      {props.description.includes('PROTECT') && <RuneProtect />}
     </div>
   );
 };
